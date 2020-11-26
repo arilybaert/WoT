77be8db99b1df7a4cc90639f1d3460ed86558037
arilybaert <arilybae@arteveldehs.be>
2020-11-26 17:41:48 +0100

    added laravel auth + changed routes to laravel-8 format

--- app/Http/Controllers/LogbookController.php
+++ app/Http/Controllers/LogbookController.php
@@ -2,17 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Students;
 use Illuminate\Http\Request;
 
 class LogbookController extends Controller
 {
-
     public function getIndex(){
 
-       /*  $students = Students::all();
-        var_dump($students); */
-        return view('pages.logbook');
+        /*  $students = Students::all();
+         var_dump($students); */
+         return view('pages.logbook');
 
-    }
+     }
 }
